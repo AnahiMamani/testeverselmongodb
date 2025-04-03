2e5cb3fcf7dd16a31383452e1403ed5a8baa2f66
AnahiMamani <narielanahi@gmail.com>
2025-04-02 22:21:07 -0300

    teste

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(express.json());
 app.use(cors());
 
 // Conectar ao MongoDB (troque pela sua string do MongoDB Atlas)
-mongoose.connect("mongodb+srv://narielanahi:<db_password>@cluster0.zkkuczc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect("mongodb+srv://Anahi:1234@cluster0.zkkuczc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 // Criar modelo de nome
@@ -18,15 +18,15 @@ const Nome = mongoose.model("Nome", NomeSchema);
 
 // Rota para adicionar nome
 app.post("/add", async (req, res) => {
-  const novoNome = new Nome({ nome: req.body.nome });
-  await novoNome.save();
-  res.json({ mensagem: "Nome salvo!" });
+    const novoNome = new Nome({ nome: req.body.nome });
+    await novoNome.save();
+    res.json({ mensagem: "Nome salvo!" });
 });
 
 // Rota para listar nomes
 app.get("/nomes", async (req, res) => {
-  const nomes = await Nome.find();
-  res.json(nomes);
+    const nomes = await Nome.find();
+    res.json(nomes);
 });
 
 // Iniciar o servidor
